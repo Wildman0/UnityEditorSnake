0d75a297ac3aba2731e94c957b5645bd492f2564
Wildman0 <kylewildman98@gmail.com>
2018-12-27 23:02:14 +0000

    Initial Commit
    Initial Commit

---
+++ Assets/Editor/EditorSnake.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class EditorSnake : EditorWindow
+{
+    public static EditorSnake window;
+    private Snake snake;
+
+    private bool gamePaused = true;
+    
+    public delegate void MovementInput(Snake.Direction dir);
+    public static event MovementInput OnMovementInput;
+    
+    private const int toggleBoxWidth = 13;
+    private const int movementPanelOffset = 50;
+    private const int verticalOffset = 20;
+    private const int horizontalOffset = 20;
+
+    private const int millisecondsBetweenTicks = 250;
+
+    private const int buttonSideLong = 40;
+    private const int buttonSideShort = 20;
+
+    private DateTime lastTickTime;
+    private TimeSpan timeToLastTickDifference;
+
+    [MenuItem("Snake/Snake")]
+    public static void ShowEditorWindow()
+    {
+        Init();
+    }
+    
+    //Runs on editor window destruction
+    void OnDestroy()
+    {
+        EditorApplication.update -= Update;
+        OnMovementInput -= UpdateMovementDirection;
+    }
+    
+    //Called on window initialization
+    static void Init()
+    {
+        window = (EditorSnake)EditorWindow.GetWindow(typeof(EditorSnake));
+        window.Show();
+    }
+
+    //Runs at start
+    void OnEnable()
+    {
+        snake = new Snake();
+        gamePaused = true;
+        AddEventSubscriptions();
+    }
+
+    //Adds all necessary event subscriptions
+    void AddEventSubscriptions()
+    {
+        EditorApplication.update += Update;
+        OnMovementInput += UpdateMovementDirection;
+    }
+
+    //Updates the current movement direction
+    void UpdateMovementDirection(Snake.Direction dir)
+    {
+        if (!IsDirectionOpposite(dir, snake.movementDirection))
+        {
+            snake.movementDirection = dir;
+        }
+    }
+
+    //Returns whether or not 2 directions are opposites
+    bool IsDirectionOpposite(Snake.Direction dir1, Snake.Direction dir2)
+    {
+        return (Mathf.Abs((int) dir1 - (int) dir2) == 2);
+    }
+
+    //Runs on any GUI update
+    void OnGUI()
+    {
+        DrawStartButton();
+        DrawBorders();
+        SetMovementButtonInputs();
+        DrawSnakeBlocks(snake.snakeBlocksPositions);
+        DrawLooseBlock();
+    }
+
+    //Draws start button and gets it's input
+    void DrawStartButton()
+    {
+        if (GUI.Button(new Rect(295, 70, 100, 30), "Start Game"))
+        {
+            gamePaused = false;
+        }
+    }
+
+    //Draws the borders of the map
+    void DrawBorders()
+    {
+        DrawHorizontalBorders();
+        DrawVerticalBorders();
+    }
+
+    //Draws the horizontal borders of the map
+    void DrawHorizontalBorders()
+    {
+        for (int i = 0; i < snake.mapSize.x + 1; i++)
+        {
+            GUI.Toggle(new Rect(toggleBoxWidth * i, 0, 0, 0), true, "");
+            GUI.Toggle(new Rect(toggleBoxWidth * i, snake.mapSize.y * toggleBoxWidth + 1, 0, 0), true, "");
+        }
+    }
+
+    //Draws the vertical borders of the map
+    void DrawVerticalBorders()
+    {
+        for (int i = 0; i < snake.mapSize.x + 1; i++)
+        {
+            GUI.Toggle(new Rect(0, toggleBoxWidth * i, 0, 0), true, ""); 
+            GUI.Toggle(new Rect(snake.mapSize.y * toggleBoxWidth + 1, toggleBoxWidth * i, 0, 0), true, "");
+        }
+    }
+
+    //Draws all snake blocks
+    void DrawSnakeBlocks(List<Vector2Int> blockPositions)
+    {
+        for (int i = 0; i < blockPositions.Count; i++)
+        {
+            GUI.Toggle(new Rect((blockPositions[i].x + 1) * toggleBoxWidth, (blockPositions[i].y + 1) * toggleBoxWidth, 0, 0), snake.movementDisabled, "");
+        }
+    }
+
+    //Draws the loose block
+    void DrawLooseBlock()
+    {
+        GUI.Toggle(new Rect((snake.looseBlock.x + 1) * toggleBoxWidth, (snake.looseBlock.y + 1) * toggleBoxWidth, 0, 0), true, "");
+    }
+
+    //Draws the movement buttons
+    void SetMovementButtonInputs()
+    {
+        Rect centerOfMovementButtons = new Rect((snake.mapSize.x + 1) * toggleBoxWidth + movementPanelOffset,
+            snake.mapSize.y / 2 * toggleBoxWidth, 0, 0);
+
+        if (GUI.Button(
+            new Rect(centerOfMovementButtons.x, centerOfMovementButtons.y - verticalOffset, buttonSideLong,
+                buttonSideShort), "^"))
+        {
+            OnMovementInput(Snake.Direction.Up);
+        }
+
+        if (GUI.Button(
+            new Rect(centerOfMovementButtons.x, centerOfMovementButtons.y + verticalOffset + buttonSideShort,
+                buttonSideLong, buttonSideShort), "v"))
+        {
+            OnMovementInput(Snake.Direction.Down);
+        }
+
+        if (GUI.Button(
+            new Rect(centerOfMovementButtons.x + horizontalOffset + buttonSideShort, centerOfMovementButtons.y,
+                buttonSideShort, buttonSideLong), ">"))
+        {
+            OnMovementInput(Snake.Direction.Right);
+        }
+
+        if (GUI.Button(
+            new Rect(centerOfMovementButtons.x - horizontalOffset, centerOfMovementButtons.y, buttonSideShort,
+                buttonSideLong), "<"))
+        {
+            OnMovementInput(Snake.Direction.Left);
+        }
+    }
+
+    //Runs every frame (kinda)
+    void Update()
+    {
+        if (DateTime.Now > lastTickTime + TimeSpan.FromMilliseconds(millisecondsBetweenTicks) && !gamePaused)
+        {
+            Tick();
+            lastTickTime = DateTime.Now;
+        }
+    }
+
+    //Runs as often as specified
+    void Tick()
+    {
+        snake.Move(snake.movementDirection);
+
+        window.Repaint();
+    }
+}
---
+++ Assets/Source/Snake.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class Snake
+{
+    public List<Vector2Int> snakeBlocksPositions;
+    public Vector2Int mapSize = new Vector2Int(20, 20);
+    public Vector2Int looseBlock = new Vector2Int(2, 7);
+
+    private const int toggleBoxWidth = 13;
+    public bool movementDisabled;
+
+    public Direction movementDirection;
+
+    public enum Direction
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    };
+
+    //Constructor
+    public Snake()
+    {
+        Start();
+    }
+
+    //Runs at start
+    void Start()
+    {
+        snakeBlocksPositions = new List<Vector2Int>();
+        SetInitialPosition();
+    }
+
+    //Sets the initial block positions to near the center
+    public void SetInitialPosition()
+    {
+        snakeBlocksPositions.Add(new Vector2Int(10, 10));
+        snakeBlocksPositions.Add(new Vector2Int(9, 10));
+        snakeBlocksPositions.Add(new Vector2Int(8, 10));
+        snakeBlocksPositions.Add(new Vector2Int(7, 10));
+    }
+
+    //Moves all of the blocks of the snake
+    public void Move(Direction dir)
+    {
+        if (!IsPositionOccupied(GetBlockPlusDirectionPosition(snakeBlocksPositions[0], movementDirection)) && !movementDisabled)
+        {
+            for (int i = snakeBlocksPositions.Count - 1; i >= 0; i--)
+            {
+                snakeBlocksPositions[i] = (i != 0) ? snakeBlocksPositions[i - 1] : GetBlockPlusDirectionPosition(snakeBlocksPositions[i], movementDirection);
+            }
+        }
+        else
+        {
+            GameOver();
+        }
+
+        CheckForBlockPickup();
+    }
+
+    //Returns whether or not a given position is occupied
+    public bool IsPositionOccupied(Vector2Int v)
+    {
+        for (int i = 0; i < snakeBlocksPositions.Count - 1; i++)
+        {
+            if (snakeBlocksPositions[i] == v)
+                return true;
+        }
+
+        return IsEdgeOfMap(v);
+    }
+
+    //Returns whether or not a given position is along the edge of the map
+    private bool IsEdgeOfMap(Vector2Int v)
+    {
+        return v.x < 0 || v.x == mapSize.x - 1 || v.y < 0 || v.y == mapSize.y - 1;
+    }
+
+    //Returns a given direction from a position
+    private Vector2Int GetBlockPlusDirectionPosition(Vector2Int v, Direction dir)
+    {
+        switch (dir)
+        {
+            case Direction.Down:
+                return new Vector2Int(v.x, v.y + 1);
+
+            case Direction.Up:
+                return new Vector2Int(v.x, v.y - 1);
+
+            case Direction.Right:
+                return new Vector2Int(v.x + 1, v.y);
+
+            case Direction.Left:
+                return new Vector2Int(v.x - 1, v.y);
+
+            default:
+                return GetBlockPlusDirectionPosition(v, movementDirection);
+        }
+    }
+
+    //Generates a new loose block, checking that it's a valid position
+    void GenerateNewBlock()
+    {
+        Vector2Int proposedPosition = new Vector2Int(Random.Range(0, mapSize.x - 1), Random.Range(0, mapSize.y - 1));
+
+        for (int i = 0; i < snakeBlocksPositions.Count - 1; i++)
+        {
+            if (snakeBlocksPositions[i] == proposedPosition)
+            {
+                GenerateNewBlock();
+                break;
+            }
+        }
+
+        looseBlock = proposedPosition;
+    }
+
+    //Checks whether or not the loose block has been picked up
+    void CheckForBlockPickup()
+    {
+        if (snakeBlocksPositions[0] == looseBlock)
+        {
+            PickupBlock();
+        }
+    }
+
+    //Adds the loose block to the snake
+    void PickupBlock()
+    {
+        GenerateNewBlock();
+
+        snakeBlocksPositions.Add(new Vector2Int(snakeBlocksPositions[snakeBlocksPositions.Count - 1].x,
+            snakeBlocksPositions[snakeBlocksPositions.Count - 1].y));
+    }
+
+    //Runs when the game ends
+    void GameOver()
+    {
+        movementDisabled = true;
+    }
+}
